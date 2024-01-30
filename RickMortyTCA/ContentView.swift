101dc0d55817ad9650cf3fddaf5f6ec787bbaf9e
BogdanZyk <b.zykov@brightlab.me>
2024-01-30 11:05:51 +0300

    Initial Commit

---
+++ RickMortyTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickMortyTCA
+//
+//  Created by Bogdan Zykov on 30.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickMortyTCA/RickMortyTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyTCAApp.swift
+//  RickMortyTCA
+//
+//  Created by Bogdan Zykov on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
